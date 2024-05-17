27fc0c6fdab69782863f5202171c8dac7c8ff43a
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-05-17 13:12:58 +0700

    update 'IsAuthorOrReadOnly' permission class

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,6 +4,4 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method not in SAFE_METHODS:
-            return request.user == obj.author
-        return True
+        return request.method in SAFE_METHODS or request.user == obj.author
